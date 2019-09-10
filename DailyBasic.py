60c710ff9d7206527d3b13e18ae2152457864700
d <d@163.com>
2019-09-10 19:27:29 +0800

    标准差因子

--- DailyBasic.py
+++ DailyBasic.py
@@ -14,13 +14,18 @@ def get_rank(ts_data_frame, factor):
     return factor_rank < quantity_30
 
 
+def get_corr(data, column):
+    data_normalization = (data[column] - data[column].min()) / (data[column].max() - data[column].min())
+    return data_normalization.std()
+
+
 class DailyBasic:
     """
     获取沪深300成分股的每日PE、PB、PS值，并计算在过去某段时间类三个值百分位值
     筛选出三个值都位于30%一下的股票，并将统计结果以邮件的方式通知到自己
     """
 
-    def __init__(self):
+    def __init__(self) -> object:
         self.pro = tushare_data.get_tushare_pro()
         trade_date = datetime.datetime.now() .strftime('%Y%m%d')
         df = self.pro.index_weight(index_code='399300.SZ')
@@ -28,6 +33,7 @@ class DailyBasic:
         df = df.head(300)
         self.hs300_ts_code = df['con_code']
         self.selected_ts = []
+        self.std = []
 
     def check_ts(self, ts_code):
         """
@@ -38,10 +44,12 @@ class DailyBasic:
         pe_perception = get_rank(ts_data_frame, 'pe_ttm')
         pb_perception = get_rank(ts_data_frame, 'pb')
         ps_perception = get_rank(ts_data_frame, 'ps_ttm')
+        close_std = get_corr(ts_data_frame, 'close')
+        self.std.append(close_std)
         if pe_perception and pb_perception and ps_perception:
-            return True
+            return close_std
         else:
-            return False
+            return None
 
     def get_today_selected_ts(self):
         """
@@ -49,9 +57,10 @@ class DailyBasic:
         :return: 三个指标值都位于30%以内的股票
         """
         for ts_code in self.hs300_ts_code:
-            if self.check_ts(ts_code):
+            std = self.check_ts(ts_code)
+            if std is not None:
                 time.sleep(1)
-                self.selected_ts.append(ts_code)
+                self.selected_ts.append([ts_code, std])
         return self.selected_ts
 
     """
@@ -59,3 +68,13 @@ class DailyBasic:
     ts_codes = dailyBasic.get_today_selected_ts()
     print(ts_codes)
     """
+
+    """
+    dailyBasic = DailyBasic()
+    selected_ts = dailyBasic.get_today_selected_ts()
+    column_name = ['ts_code', 'std']
+    from pandas import DataFrame as dff
+    data_frame = dff(columns=column_name, data=selected_ts)
+    data_frame.to_csv('./stocks.csv', encoding='utf-8')
+    """
+
